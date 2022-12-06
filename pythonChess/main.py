d9615ac173f1caef596f68860e12dda19400ebfd
Andrii <ramp8a799@gmail.com>
2022-12-06 18:21:44 +0200

    added negascout and supportive functions

--- pythonChess/main.py
+++ pythonChess/main.py
@@ -1,9 +1,11 @@
 from math import inf
 import chess
 import constants
+
 board = chess.Board()
 
-class ChessNegaMax:
+
+class ChessAgent:
     whites = ['P', 'N', "R", "Q", "B"]
     blacks = ["p", "n", "r", "q", "b"]
 
@@ -34,8 +36,8 @@ class ChessNegaMax:
         return weight
 
     def evaluationFunction(self, gameState: chess.Board(), whoToGo) -> int:
-        numOfAllWhites = self.piecesCountHelper(gameState, "white")
-        numOfAllBlacks = self.piecesCountHelper(gameState, "black")
+        numOfAllWhites = 19
+        numOfAllBlacks = 18
         materialWeight = self.materialWeightEvaluationHelper(gameState)
 
         return materialWeight * (numOfAllWhites - numOfAllBlacks) * whoToGo
@@ -60,30 +62,35 @@ class ChessNegaMax:
 
         return maxScore, bestMove
 
-    def negaScout(self,gameState: chess.Board(), depth, whoToGo, alpha, beta):
+    def negaScout(self, gameState: chess.Board(), depth, whoToGo, alpha, beta):
         typesOfDraw = gameState.can_claim_draw() or gameState.can_claim_fifty_moves() \
                       or gameState.is_insufficient_material() or gameState.is_stalemate()
 
         if depth == 0 or typesOfDraw or gameState.is_checkmate():
             return self.evaluationFunction(gameState, whoToGo), None
 
-        maxScore = -inf
-        legalMoves = gameState.legal_moves
         bestMove = None
-        for move,i in  zip(legalMoves, range(len(legalMoves))):
+        legalMoves = gameState.legal_moves
+        a = alpha
+        b = beta
+
+        for move, i in zip(legalMoves, range(legalMoves.count())):
             gameState.push_san(move.uci())
-            score = -self.negaScout(gameState, depth - 1, whoToGo * -1)[0]
-
-            if score > alpha and score < beta and i > 1 and depth == 1:
-                a = -self.negaScout(gameState, depth - 1, whoToGo * -1, -beta, -score)
-
+            score = -self.negaScout(gameState, depth - 1, whoToGo * -1, -b, -a)[0]
+            if a < score < b and i > 1 and depth == 1:
+                a = -self.negaScout(gameState, depth - 1, whoToGo * -1, -beta, -score)[0]
+            a = max(a, score)
+            if a == score:
+                bestMove = move
+            if a >= beta:
+                return a, move
+            b = a + 1
             gameState.pop()
+        return a, bestMove
 
 
+chessAgent = ChessAgent()
 
-chessNegaMax = ChessNegaMax()
+bestMoveScore = chessAgent.negaMax(board, 3, 1)
 
-bestMoveScore = chessNegaMax.negaMax(board, 3, 1)
-
-print(bestMoveScore[1])
-
+print(bestMoveScore)
